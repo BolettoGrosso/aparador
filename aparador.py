c61ade8f40efb3de3e1b3da1997bd822c1cff703
Tomàs Gea <32648506+BolettoGrosso@users.noreply.github.com>
2025-01-01 19:07:54 +0100

    Update aparador.py

--- aparador.py
+++ aparador.py
@@ -20,12 +20,12 @@ st.title('Google Sheets Data Viewer')
 data = load_data()
 
 # Sorting options
-sort_column = st.selectbox("Sort by:", data.columns)
-sort_order = st.radio("Order:", ["Ascending", "Descending"])
+#sort_column = st.selectbox("Sort by:", data.columns)
+#sort_order = st.radio("Order:", ["Ascending", "Descending"])
 
 # Sort data based on user selection
-ascending = True if sort_order == "Ascending" else False
-data = data.sort_values(by=sort_column, ascending=ascending)
+#ascending = True if sort_order == "Ascending" else False
+#data = data.sort_values(by=sort_column, ascending=ascending)
 
 # Display the first 10 rows
 st.write('### First 10 Rows of the Dataset')
