80c1ee3b3a10f6cd5d9b6a612bbc379436f72e0b
Tomàs Gea <32648506+BolettoGrosso@users.noreply.github.com>
2025-01-01 19:10:37 +0100

    Update aparador.py

--- aparador.py
+++ aparador.py
@@ -9,7 +9,6 @@ sheet_url = 'https://docs.google.com/spreadsheets/d/1Hu2Rl6QE6-Fd_MeP2M4Ffb6Xzcm
 def load_data():
     data = pd.read_csv(sheet_url)
     # Debugging: Show all columns to identify the correct names
-    #st.write("Columns in dataset:", data.columns.tolist())
     # Replace these with the correct column names based on the output above
     columns_to_display = ["Identificador", "Fecha de conclusión", "Valor subasta", "Provincia_lot'"]  # Replace with real names
     data = data[columns_to_display]
@@ -18,14 +17,6 @@ def load_data():
 # Load the dataset
 st.title('Subastas disponibles')
 data = load_data()
-
-# Sorting options
-#sort_column = st.selectbox("Sort by:", data.columns)
-#sort_order = st.radio("Order:", ["Ascending", "Descending"])
-
-# Sort data based on user selection
-#ascending = True if sort_order == "Ascending" else False
-#data = data.sort_values(by=sort_column, ascending=ascending)
 
 # Display the first 10 rows
 st.write('### First 10 Rows of the Dataset')
